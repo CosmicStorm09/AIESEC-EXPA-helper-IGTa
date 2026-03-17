a6b3f5a27bf35f414f8b98c339d8e100792d2fcf
Harsh <119665294+CosmicStorm09@users.noreply.github.com>
2026-03-17 17:44:00 +0530

    Update 2_Data_Verification.py

--- pages/2_Data_Verification.py
+++ pages/2_Data_Verification.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Data Verification Tool")
+
+uploaded_file = st.file_uploader(
+    "Upload Verification Dataset",
+    type=["csv"]
+)
+
+if uploaded_file:
+
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Dataset Preview")
+
+    st.dataframe(df)
+
+    st.write("Add verification checks here")
